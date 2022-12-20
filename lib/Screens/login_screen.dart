0e793bc417f460ef80b9658830a9864c4697ec8b
Md Nahid Hossen Golap <81738523+nrosebd@users.noreply.github.com>
2022-12-20 19:55:43 +0600

    add back button to login screen

--- lib/Screens/login_screen.dart
+++ lib/Screens/login_screen.dart
@@ -17,113 +17,134 @@ class LoginScreen extends StatelessWidget {
           alignment: Alignment.center,
           children: <Widget>[
             Positioned(
-                top: 0,
-                left: 0,
-                child: Image.asset(
-                  "assets/images/signup_top.png",
-                  width: size.width * 0.3,
-                )
+              top: 0,
+              left: 0,
+              child: Image.asset(
+                "assets/images/signup_top.png",
+                width: size.width * 0.3,
+              )
             ),
             Positioned(
-                bottom: 0,
-                right: 0,
-                child: Image.asset(
-                  "assets/images/login_bottom.png",
-                  width: size.width * 0.3,
-                )
+              bottom: 0,
+              right: 0,
+              child: Image.asset(
+                "assets/images/login_bottom.png",
+                width: size.width * 0.3,
+              )
+            ),
+            Positioned(
+              top: 35,
+              left: 10,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(50),
+                child: TextButton(
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(primaryLightColor),
+                  ),
+                  onPressed: () { Navigator.pop(context); },
+                  child: const SizedBox(
+                    width: 50,
+                    height: 50,
+                    child: Icon(Icons.arrow_back, color: primaryColor,),
+                  )
+                ),
+              ),
             ),
             SafeArea(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text(
-                    "Login".toUpperCase(),
-                    style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-                  ),
-                  const SizedBox(height: 25,),
-                  SvgPicture.asset(
-                    "assets/icons/login.svg",
-                    height: size.height * 0.37,
-                  ),
-                  const SizedBox(height: 10,),
-                  SizedBox(
-                    width: size.width * 0.8,
-                    child: Column(
-                      children: [
-                        Container(
-                          padding: const EdgeInsets.symmetric(horizontal: 15),
-                          decoration: BoxDecoration(
-                              color: primaryLightColor,
-                              borderRadius: BorderRadius.circular(30)
-                          ),
-                          child: const TextField(
-                            decoration: InputDecoration(
-                              icon: Icon(Icons.person, color: primaryColor,),
-                              hintText: "Your Email",
-                              border: InputBorder.none,
-                            ),
-                          ),
-                        ),
-                        const SizedBox(height: 5,),
-                        Container(
-                          padding: const EdgeInsets.symmetric(horizontal: 15),
-                          decoration: BoxDecoration(
-                              color: primaryLightColor,
-                              borderRadius: BorderRadius.circular(30)
-                          ),
-                          child: const TextField(
-                            obscureText: true,
-                            decoration: InputDecoration(
-                              icon: Icon(
-                                Icons.lock,
-                                color: primaryColor,
-                              ),
-                              suffixIcon: Icon(
-                                Icons.remove_red_eye_sharp,
-                                color: primaryColor,
-                              ),
-                              hintText: "Your Password",
-                              border: InputBorder.none,
-                            ),
-                          ),
-                        ),
-                      ],
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      "Login".toUpperCase(),
+                      style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
                     ),
-                  ),
-                  const SizedBox(height: 30,),
-                  SizedBox(
-                    width: size.width * 0.8,
-                    height: 50,
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(50),
-                      child: TextButton(
-                        onPressed: () {},
-                        style: ButtonStyle(backgroundColor: MaterialStateProperty.all(primaryColor)),
-                        child: Text(
-                          "Login".toUpperCase(),
-                          style: const TextStyle(color: Colors.white),
+                    const SizedBox(height: 25,),
+                    SvgPicture.asset(
+                      "assets/icons/login.svg",
+                      height: size.height * 0.37,
+                    ),
+                    const SizedBox(height: 10,),
+                    SizedBox(
+                      width: size.width * 0.8,
+                      child: Column(
+                        children: [
+                          Container(
+                            padding: const EdgeInsets.symmetric(horizontal: 15),
+                            decoration: BoxDecoration(
+                                color: primaryLightColor,
+                                borderRadius: BorderRadius.circular(30)
+                            ),
+                            child: const TextField(
+                              decoration: InputDecoration(
+                                icon: Icon(Icons.person, color: primaryColor,),
+                                hintText: "Your Email",
+                                border: InputBorder.none,
+                              ),
+                            ),
+                          ),
+                          const SizedBox(height: 5,),
+                          Container(
+                            padding: const EdgeInsets.symmetric(horizontal: 15),
+                            decoration: BoxDecoration(
+                                color: primaryLightColor,
+                                borderRadius: BorderRadius.circular(30)
+                            ),
+                            child: const TextField(
+                              obscureText: true,
+                              decoration: InputDecoration(
+                                icon: Icon(
+                                  Icons.lock,
+                                  color: primaryColor,
+                                ),
+                                suffixIcon: Icon(
+                                  Icons.remove_red_eye_sharp,
+                                  color: primaryColor,
+                                ),
+                                hintText: "Your Password",
+                                border: InputBorder.none,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(height: 30,),
+                    SizedBox(
+                      width: size.width * 0.8,
+                      height: 50,
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(50),
+                        child: TextButton(
+                          onPressed: () {},
+                          style: ButtonStyle(backgroundColor: MaterialStateProperty.all(primaryColor)),
+                          child: Text(
+                            "Login".toUpperCase(),
+                            style: const TextStyle(color: Colors.white),
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                  Container(
-                    alignment: Alignment.center,
-                    width: size.width * 0.8,
-                    height: 50,
-                    child: Row(
-                      children: [
-                        const Text("Don't have an Account?"),
-                        TextButton(
-                            onPressed: () {},
-                            child: const Text(
-                              "Sign Up"
-                            )
-                        )
-                      ],
+                    Container(
+                      alignment: Alignment.center,
+                      width: size.width * 0.8,
+                      height: 50,
+                      child: Row(
+                        children: [
+                          const Text("Don't have an Account?"),
+                          TextButton(
+                              onPressed: () {},
+                              child: const Text(
+                                  "Sign Up"
+                              )
+                          )
+                        ],
+                      ),
                     ),
-                  ),
-                ],
-              ),
+                  ],
+                ),
+              )
+
             ),
           ],
         ),
